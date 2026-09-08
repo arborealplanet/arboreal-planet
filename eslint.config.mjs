e3328835710020392c69dc00b5b48580cc0429fa
arborealplanet <arborealplanet@gmail.com>
2026-09-08 11:42:05 -0400

    Scope breeder clock purity exception to game components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,15 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: [
+      "src/components/ChondroBreederGame.tsx",
+      "src/components/ChondroBreederGameV3.tsx",
+    ],
+    rules: {
+      "react-hooks/purity": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
