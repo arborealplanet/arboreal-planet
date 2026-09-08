a4252a5157dc1fba50939987799bb9e41e0f42c9
arborealplanet <arborealplanet@gmail.com>
2026-09-08 11:58:52 -0400

    Add breeder friends and showcase to game page

--- src/app/hatchery/chondro-breeder/page.tsx
+++ src/app/hatchery/chondro-breeder/page.tsx
@@ -1,5 +1,11 @@
 import { ChondroBreederGameV3 } from "@/components/ChondroBreederGameV3";
+import { ChondroBreederSocial } from "@/components/ChondroBreederSocial";
 
 export default function ChondroBreederPage() {
-  return <ChondroBreederGameV3 />;
+  return (
+    <>
+      <ChondroBreederGameV3 />
+      <ChondroBreederSocial />
+    </>
+  );
 }
