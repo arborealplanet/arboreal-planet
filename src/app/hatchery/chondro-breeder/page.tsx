d6fa98cc574e67ab0270554e299dfedb2d56d845
arborealplanet <arborealplanet@gmail.com>
2026-09-08 19:21:55 -0400

    Add favorites and market recovery tools

--- src/app/hatchery/chondro-breeder/page.tsx
+++ src/app/hatchery/chondro-breeder/page.tsx
@@ -7,12 +7,14 @@ import { ChondroBreederFacility } from "@/components/ChondroBreederFacility";
 import { ChondroBreederSubspeciesPhenotypes } from "@/components/ChondroBreederSubspeciesPhenotypes";
 import { ChondroConservationPartnerships } from "@/components/ChondroConservationPartnerships";
 import { ChondroTraitFocusPanel } from "@/components/ChondroTraitFocusPanel";
+import { ChondroFavoritesMarketPanel } from "@/components/ChondroFavoritesMarketPanel";
 
 export default function ChondroBreederPage() {
   return (
     <>
       <ChondroBreederSubspeciesPhenotypes />
       <ChondroTraitFocusPanel />
+      <ChondroFavoritesMarketPanel />
       <ChondroBreederGameV3 />
       <ChondroBreederExpandedShop />
       <ChondroConservationPartnerships />
