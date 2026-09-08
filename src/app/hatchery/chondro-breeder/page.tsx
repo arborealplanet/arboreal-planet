d16cd4f3afdbc4fc33df73ce0b7a444a95812cf2
arborealplanet <arborealplanet@gmail.com>
2026-09-08 12:04:14 -0400

    Add customizable breeder facility to game page

--- src/app/hatchery/chondro-breeder/page.tsx
+++ src/app/hatchery/chondro-breeder/page.tsx
@@ -1,11 +1,13 @@
 import { ChondroBreederGameV3 } from "@/components/ChondroBreederGameV3";
 import { ChondroBreederSocial } from "@/components/ChondroBreederSocial";
+import { ChondroBreederFacility } from "@/components/ChondroBreederFacility";
 
 export default function ChondroBreederPage() {
   return (
     <>
       <ChondroBreederGameV3 />
       <ChondroBreederSocial />
+      <ChondroBreederFacility />
     </>
   );
 }
