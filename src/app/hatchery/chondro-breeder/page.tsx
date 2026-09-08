086a9a0f7b30321714316cc9dab728d94b0a1bd8
arborealplanet <arborealplanet@gmail.com>
2026-09-08 19:56:11 -0400

    Wire facility enclosure cap guard

--- src/app/hatchery/chondro-breeder/page.tsx
+++ src/app/hatchery/chondro-breeder/page.tsx
@@ -2,12 +2,14 @@ import { ChondroBreederGameV3 } from "@/components/ChondroBreederGameV3";
 import { ChondroBreederExpandedShop } from "@/components/ChondroBreederExpandedShop";
 import { ChondroBreederSubspeciesPhenotypes } from "@/components/ChondroBreederSubspeciesPhenotypes";
 import { ChondroFavoriteCardControls } from "@/components/ChondroFavoriteCardControls";
+import { ChondroFacilityEnclosureGuard } from "@/components/ChondroFacilityEnclosureGuard";
 import { ChondroBreederCommandCenter } from "@/components/ChondroBreederCommandCenter";
 
 export default function ChondroBreederPage() {
   return (
     <>
       <ChondroFavoriteCardControls />
+      <ChondroFacilityEnclosureGuard />
       <ChondroBreederSubspeciesPhenotypes />
       <ChondroBreederCommandCenter />
       <ChondroBreederGameV3 />
