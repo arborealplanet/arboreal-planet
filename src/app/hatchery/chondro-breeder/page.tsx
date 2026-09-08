bafc774f5b5a26ad7c7ddaac8484a1ebbca430de
arborealplanet <arborealplanet@gmail.com>
2026-09-08 19:52:42 -0400

    Compact breeder page into command center

--- src/app/hatchery/chondro-breeder/page.tsx
+++ src/app/hatchery/chondro-breeder/page.tsx
@@ -1,33 +1,17 @@
 import { ChondroBreederGameV3 } from "@/components/ChondroBreederGameV3";
 import { ChondroBreederExpandedShop } from "@/components/ChondroBreederExpandedShop";
-import { ChondroBreederLines } from "@/components/ChondroBreederLines";
-import { ChondroBreederProgression } from "@/components/ChondroBreederProgression";
-import { ChondroBreederSocial } from "@/components/ChondroBreederSocial";
-import { ChondroBreederFacility } from "@/components/ChondroBreederFacility";
 import { ChondroBreederSubspeciesPhenotypes } from "@/components/ChondroBreederSubspeciesPhenotypes";
-import { ChondroConservationPartnerships } from "@/components/ChondroConservationPartnerships";
-import { ChondroTraitFocusPanel } from "@/components/ChondroTraitFocusPanel";
-import { ChondroFavoritesMarketPanel } from "@/components/ChondroFavoritesMarketPanel";
 import { ChondroFavoriteCardControls } from "@/components/ChondroFavoriteCardControls";
-import { ChondroCollectionManager } from "@/components/ChondroCollectionManager";
-import { ChondroAchievementsPanel } from "@/components/ChondroAchievementsPanel";
+import { ChondroBreederCommandCenter } from "@/components/ChondroBreederCommandCenter";
 
 export default function ChondroBreederPage() {
   return (
     <>
       <ChondroFavoriteCardControls />
       <ChondroBreederSubspeciesPhenotypes />
-      <ChondroTraitFocusPanel />
-      <ChondroFavoritesMarketPanel />
-      <ChondroCollectionManager />
-      <ChondroAchievementsPanel />
+      <ChondroBreederCommandCenter />
       <ChondroBreederGameV3 />
       <ChondroBreederExpandedShop />
-      <ChondroConservationPartnerships />
-      <ChondroBreederLines />
-      <ChondroBreederProgression />
-      <ChondroBreederSocial />
-      <ChondroBreederFacility />
     </>
   );
 }
