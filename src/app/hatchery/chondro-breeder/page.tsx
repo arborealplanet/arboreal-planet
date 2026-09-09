6554a151a1eddc6eb25852e39aef49affffdbbc0
arborealplanet <arborealplanet@gmail.com>
2026-09-09 14:09:59 -0400

    Recover saves before mounting Chondro Breeder

--- src/app/hatchery/chondro-breeder/page.tsx
+++ src/app/hatchery/chondro-breeder/page.tsx
@@ -1,13 +1,14 @@
 import { ChondroFavoriteCardControls } from "@/components/ChondroFavoriteCardControls";
 import { ChondroAnimalCardCollapse } from "@/components/ChondroAnimalCardCollapse";
 import { ChondroBreederWorkspace } from "@/components/ChondroBreederWorkspace";
+import { ChondroSaveRecoveryGate } from "@/components/ChondroSaveRecoveryGate";
 
 export default function ChondroBreederPage() {
   return (
-    <>
+    <ChondroSaveRecoveryGate>
       <ChondroFavoriteCardControls />
       <ChondroAnimalCardCollapse />
       <ChondroBreederWorkspace />
-    </>
+    </ChondroSaveRecoveryGate>
   );
 }
