ee3b5def0da7001b514a79f16326a41a3d2afcd4
arborealplanet <arborealplanet@gmail.com>
2026-09-08 20:01:35 -0400

    Use compact breeder workspace shell

--- src/app/hatchery/chondro-breeder/page.tsx
+++ src/app/hatchery/chondro-breeder/page.tsx
@@ -1,19 +1,13 @@
-import { ChondroBreederGameV3 } from "@/components/ChondroBreederGameV3";
-import { ChondroBreederExpandedShop } from "@/components/ChondroBreederExpandedShop";
-import { ChondroBreederSubspeciesPhenotypes } from "@/components/ChondroBreederSubspeciesPhenotypes";
 import { ChondroFavoriteCardControls } from "@/components/ChondroFavoriteCardControls";
 import { ChondroFacilityEnclosureGuard } from "@/components/ChondroFacilityEnclosureGuard";
-import { ChondroBreederCommandCenter } from "@/components/ChondroBreederCommandCenter";
+import { ChondroBreederWorkspace } from "@/components/ChondroBreederWorkspace";
 
 export default function ChondroBreederPage() {
   return (
     <>
       <ChondroFavoriteCardControls />
       <ChondroFacilityEnclosureGuard />
-      <ChondroBreederSubspeciesPhenotypes />
-      <ChondroBreederCommandCenter />
-      <ChondroBreederGameV3 />
-      <ChondroBreederExpandedShop />
+      <ChondroBreederWorkspace />
     </>
   );
 }
