21e3d6333a3faa413670c5d3e13de94ba2624cff
arborealplanet <arborealplanet@gmail.com>
2026-09-14 21:22:18 -0400

    Bust cache for sharper chondro neonate assets

--- src/components/ChondroSnakeIcon.tsx
+++ src/components/ChondroSnakeIcon.tsx
@@ -4,7 +4,7 @@ type PortraitTraits = Partial<Record<TraitKey, number>> & { blue?: number };
 type LifeStage = "Hatchling" | "Neonate" | "Subadult" | "Adult";
 type NeonateColor = "Red" | "Yellow";
 
-const TRAIT_ART_VERSION = "2026-09-09-i";
+const TRAIT_ART_VERSION = "2026-09-15-neonate-sharp";
 
 const baseArtBySubspecies: Record<ChondroSubspecies, string> = {
   "Morelia azurea azurea": "/hatchery/snakes/azurea.avif",
