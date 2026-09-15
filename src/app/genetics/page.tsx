de6ad9a4e276be5c2dcf723f3633b65763f304fb
arborealplanet <arborealplanet@gmail.com>
2026-09-15 11:51:54 -0400

    Add pedigree ownership transfer center

--- src/app/genetics/page.tsx
+++ src/app/genetics/page.tsx
@@ -4,6 +4,7 @@ import { GtpFamilyTreeMaker } from "@/components/GtpFamilyTreeMaker";
 import { GtpLocalityConfidenceGuide } from "@/components/GtpLocalityConfidenceGuide";
 import { GtpPedigreeCloudSync } from "@/components/GtpPedigreeCloudSync";
 import { GtpPedigreePublishing } from "@/components/GtpPedigreePublishing";
+import { GtpPedigreeTransfers } from "@/components/GtpPedigreeTransfers";
 import { GtpSubspeciesCalculator } from "@/components/GtpSubspeciesCalculator";
 
 export default function GeneticsCalculatorPage() {
@@ -30,6 +31,10 @@ export default function GeneticsCalculatorPage() {
 
       <section className="mx-auto max-w-7xl px-5 pb-5 sm:px-6">
         <GtpPedigreePublishing />
+      </section>
+
+      <section className="mx-auto max-w-7xl px-5 pb-5 sm:px-6">
+        <GtpPedigreeTransfers />
       </section>
 
       <section className="mx-auto max-w-7xl px-5 pb-16 sm:px-6 lg:pb-20">
