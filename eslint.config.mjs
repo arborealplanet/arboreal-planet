32507cc354cc3f500ad53492e7d821d5e8c22584
arborealplanet <arborealplanet@gmail.com>
2026-09-15 11:55:05 -0400

    Allow pedigree transfer hydration effect

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,6 +33,7 @@ const eslintConfig = defineConfig([
       "src/components/GtpFamilyTreeMaker.tsx",
       "src/components/GtpPedigreeCloudSync.tsx",
       "src/components/GtpPedigreePublishing.tsx",
+      "src/components/GtpPedigreeTransfers.tsx",
       "src/components/GtpPublicLineageDatabase.tsx",
     ],
     rules: {
